8471b0558f30e243b4bd3c7beaedcf2019e9df83
ccali14 <calicagatay@icloud.com>
2016-08-23 19:15:35 -0700

    Readme

---
+++ index.js
@@ -0,0 +1,16 @@
+var I = require('1n1t');
+var E = require('3x3c');
+var async = require('async');
+
+module.exports = function (message) {
+ return new Promise(function(resolve, reject) {
+   I()
+   .then((value) => {
+     console.log(value);
+     E(`git add . && git commit -m ${message} && git push origin ${value}`)
+       .then((value) => {resolve(value);})
+       .catch((err) => {reject(err);})
+   })
+   .catch((err) => {reject(err);})
+ });
+}
