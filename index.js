1fa9a855ddcee0647070ddac8ed8b3a3b686b88c
cagataycali <calicagatay@icloud.com>
2016-08-24 18:11:46 -0700

    Notifier added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var I = require('1n1t');
 var E = require('3x3c');
 var async = require('async');
+var updateNotifier = require('update-notifier');
+var pkg = require('./package.json');
+updateNotifier({pkg}).notify();
 
 module.exports = function (message) {
  return new Promise(function(resolve, reject) {
    I()
    .then((value) => {
      console.log(value);
-     E(`git add . && git commit -m "${message}" && git push origin "${value}"`)
+     E(`git add . && git commit -m "${message}" && git push origin "${value.trim()}"`)
        .then((value) => {resolve(value);})
        .catch((err) => {reject(err);})
    })
