f82dc27ade60e4677b3602a998f217571e138344
cagataycali <calicagatay@icloud.com>
2016-08-24 22:13:37 -0700

    Branch fix

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ var updateNotifier = require('update-notifier');
 var pkg = require('./package.json');
 updateNotifier({pkg}).notify();
 
-module.exports = function (message) {
+module.exports = function (message, branch) {
  return new Promise(function(resolve, reject) {
    I()
    .then((value) => {
      console.log(value);
-     E(`git add . && git commit -m "${message}" && git push origin "${value.trim()}"`)
+     var commitBranch;
+     if (branch) {
+       commitBranch = branch;
+     } else {
+       commitBranch = value.trim();
+     }
+     E(`git add . && git commit -m "${message}" && git push origin "${commitBranch}"`)
        .then((value) => {resolve(value);})
        .catch((err) => {reject(err);})
    })
