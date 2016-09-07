cd420b9a5a8f2b1c9402338e6d015e9355af835f
cagataycali <calicagatay@icloud.com>
2016-09-06 18:54:13 -0700

    Rm npm debug log

--- demo.js
+++ demo.js
@@ -1,9 +1,5 @@
 var C = require('./index');
 
 C('Bug fixes')
-  .then(function (value) {
-    console.log('Process end.');
-  })
-  .catch(function (err) {
-
-  })
+  .then(function (value) {console.log(value);})
+  .catch(function (err) {console.log(err);})
